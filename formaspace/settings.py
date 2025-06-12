46e861b5457904452dfb146a7a7bc72185502092
olutobi1996 <olutobi1996@icloud.com>
2025-06-12 16:37:27 +0100

    updated home template

--- formaspace/settings.py
+++ formaspace/settings.py
@@ -30,7 +30,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -127,3 +126,4 @@ USE_TZ = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
