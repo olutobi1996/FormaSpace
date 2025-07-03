250babb60c99226ed4dbdbd946dcf9b38cc8413a
olutobi1996 <olutobi1996@icloud.com>
2025-07-03 21:06:25 +0100

    virtual office, co-working, private office added

--- offices/urls.py
+++ offices/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'offices'
 
+# offices/urls.py
 urlpatterns = [
-    path('', views.location_list, name='location_list'),
-    path('<int:pk>/', views.location_detail, name='location_detail'),
+    path('co-working/', views.co_working, name='co_working'),
+    path('private-office/', views.private_office, name='private_office'),
+    path('virtual-office/', views.virtual_office, name='virtual_office'),
 ]
+
+
+
--- offices/views.py
+++ offices/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Location
 
-def location_list(request):
-    locations = Location.objects.all()
-    return render(request, 'offices/location_list.html', {'locations': locations})
 
-def location_detail(request, pk):
-    location = get_object_or_404(Location, pk=pk)
-    return render(request, 'offices/location_detail.html', {'location': location})
+def private_office(request):
+    return render(request, 'offices/private_office.html', {'video': True})  
 
+
+def co_working(request):
+    context = {
+        'video': True,  # or False if you want to control this dynamically
+    }
+    return render(request, 'offices/co-working.html', context)
+    
+
+def virtual_office(request):
+    return render(request, 'offices/virtual_office.html', {'video': True})
