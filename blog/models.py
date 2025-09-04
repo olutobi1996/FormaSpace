4a7b9f46dc0f3113fd0097e9c09a679c52e8800d
olutobi1996 <olutobi1996@icloud.com>
2025-09-04 21:18:52 +0100

    blog post fix

--- blog/models.py
+++ blog/models.py
@@ -10,9 +10,8 @@ class Category(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
     image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
-    content = RichTextField(blank=True, null=True)
+    content = RichTextField(blank=True, null=True)  # CKEditor field
     categories = models.ManyToManyField(Category, blank=True)
     published_at = models.DateTimeField(auto_now_add=True)
     tags = models.CharField(max_length=200, blank=True)
@@ -21,8 +20,8 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        # Adjust 'blog:post_detail' to your URL pattern name
         return reverse('blog:post_detail', args=[self.id])
+
 
 class Event(models.Model):
     title = models.CharField(max_length=200)
