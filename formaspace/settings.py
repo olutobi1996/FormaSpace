cc8c96af47dcce0e61383b93796bd1983987f0cd
olutobi1996 <olutobi1996@icloud.com>
2025-09-04 18:13:18 +0100

    email and brochure added

--- formaspace/settings.py
+++ formaspace/settings.py
@@ -141,10 +141,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.zoho.eu'  
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")  # your Gmail address
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")  # your Gmail App Password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
+# Optional: debug print to confirm
+print("EMAIL_HOST_USER:", EMAIL_HOST_USER)
+print("EMAIL_HOST_PASSWORD:", EMAIL_HOST_PASSWORD)
